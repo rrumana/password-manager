6d4ec35701a25357f89124cdea7a549ddc38bc36
rrumana <ryan.rumana@gmail.com>
2024-04-18 12:57:11 -0700

    Added test coveraage for password.rs

--- src/password.rs
+++ src/password.rs
@@ -2,17 +2,15 @@ use crate::crypto;
 
 use std::io::{Write, Read, BufWriter, BufReader};
 use std::fs::File;
+use std::path::Path;
 use anyhow::Result;
 use aes_gcm_siv::Nonce;
 use serde::{Serialize, Deserialize};
 use rusqlite::Connection;
 
-#[derive(Debug, Serialize, Deserialize)]
-pub struct Obj {
-    pub passwords: Vec<Password>,
-}
-
-
+// A custom struct to store the service and password for each entry
+// This represents one row in the user's table in the database
+// Encrypted and saved to disk
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Password {
     pub id: i32,
@@ -20,6 +18,8 @@ pub struct Password {
     pub password: String,
 }
 
+// A custom struct to store the username and nonce for each user
+// saved to disk as is
 #[derive(Debug, Serialize, Deserialize)]
 pub struct UserNonce {
     pub username: String,
@@ -44,7 +44,7 @@ pub fn create_database() -> Result<Connection> {
     Ok(conn)
 }
 
-pub fn _delete_database(conn: &Connection) -> Result<()> {
+pub fn delete_database(conn: &Connection) -> Result<()> {
     conn.execute(
         "DROP TABLE IF EXISTS passwords",
         (),
@@ -60,7 +60,7 @@ pub fn insert_password(conn: &Connection, service: &str, password: &str) -> Resu
     Ok(())
 }
 
-pub fn _update_password(conn: &Connection, service: &str, password: &str) -> Result<()> {
+pub fn update_password(conn: &Connection, service: &str, password: &str) -> Result<()> {
     conn.execute(
         "UPDATE passwords SET password = ?1 WHERE service = ?2",
         &[password, service],
@@ -68,7 +68,32 @@ pub fn _update_password(conn: &Connection, service: &str, password: &str) -> Res
     Ok(())
 }
 
-pub fn _delete_password(conn: &Connection, service: &str) -> Result<()> {
+pub fn get_password(conn: &Connection, service: &str) -> Result<String> {
+    // prepare sql query to select all rows where the service matches the input
+    let query = format!("Select * FROM passwords WHERE service = '{}'", service);
+    let mut stmt = conn.prepare(&query)?;
+
+    // execute the statement and map the results to a Password struct
+    let rows = stmt.query_map([], |row| {
+        Ok(Password {
+            id: row.get(0)?,
+            service: row.get(1)?,
+            password: row.get(2)?,
+        })
+    })?;
+
+    // iterate over results and return the password
+    // if more than one password is found, discard all but first (Temporary measure)
+    // if no password is found, return an error
+    let password: Password = match rows.into_iter().nth(0) {
+        Some(password) => password?,
+        None => return Err(anyhow::anyhow!("No password found")),
+    };
+
+    Ok(password.password)
+}
+
+pub fn delete_password(conn: &Connection, service: &str) -> Result<()> {
     conn.execute(
         "DELETE FROM passwords WHERE service = ?1",
         &[service],
@@ -156,6 +181,25 @@ pub fn load_database(username: &str) -> Result<Connection> {
 
     // if successful return the connection
     Ok(conn)
+}
+
+pub fn delete_user(username: &str) -> Result<()> {
+    // check if typical database file exists, if so delete it
+    if Path::new(&format!("{}.db", username)).exists() {
+        std::fs::remove_file(format!("{}.db", username))?;
+    }
+    // check if encrypted database file exists, if so delete it
+    if Path::new(&format!("{}.db.enc", username)).exists()  {
+        std::fs::remove_file(format!("{}.db.enc", username))?;
+    }
+
+    // check if nonce file exists, if so delete it
+    if Path::new(&format!("{}_nonce.txt", username)).exists() {
+        std::fs::remove_file(format!("{}_nonce.txt", username))?;
+    }
+
+    // return ok if successful
+    Ok(())
 }
 
 pub fn encrypt_database(username: &str, symmetric_key: &[u8; 32]) -> Result<()> {
@@ -246,4 +290,100 @@ pub fn decrypt_database(username: &str, symmetric_key: &[u8; 32]) -> Result<()> 
 
     // return Ok if successful
     Ok(())
+}
+
+#[cfg(test)]
+mod password_tests {
+    use super::*;
+
+    // This test ensures program doesn't blow up when databases are be created
+    #[test]
+    fn test_create() {
+        assert!(create_database().is_ok());
+    }
+
+    // This test ensures databases can be inserted into and verifies the output
+    #[test]
+    fn test_insert() {
+        let conn = create_database().unwrap();
+        assert!(insert_password(&conn, "password_manager", "password123").is_ok());
+        assert!(get_password(&conn, "password_manager").is_ok()); 
+        assert!(get_password(&conn, "password_manager").unwrap() == "password123");
+    }
+
+    // This test ensures rows can be updated and verifies the output
+    #[test]
+    fn test_row_update() {
+        let conn = create_database().unwrap();
+        let _ = insert_password(&conn, "password_manager", "password123").unwrap();
+        assert!(get_password(&conn, "password_manager").is_ok()); 
+        assert!(get_password(&conn, "password_manager").unwrap() == "password123");
+        assert!(update_password(&conn, "password_manager", "password456").is_ok());
+        assert!(get_password(&conn, "password_manager").unwrap() == "password456");
+    }
+
+    // This test ensures rows can be deleted and verifies the output
+    #[test]
+    fn test_row_delete() {
+        let conn = create_database().unwrap();
+        let _ = insert_password(&conn, "password_manager", "password123").unwrap();
+        assert!(get_password(&conn, "password_manager").is_ok()); 
+        assert!(get_password(&conn, "password_manager").unwrap() == "password123");
+        assert!(delete_password(&conn, "password_manager").is_ok());
+        assert!(get_password(&conn, "password_manager").is_err());
+    }
+
+    // This test ensures databases can be deleted correctly
+    #[test]
+    fn test_table_delete() {
+        let conn = create_database().unwrap();
+        let _ = insert_password(&conn, "password_manager", "password123").unwrap();
+        assert!(delete_database(&conn).is_ok());
+        assert!(get_password(&conn, "password_manager").is_err());
+    }
+
+    // This test ensures databases can be saved and loaded properly and verifies the data
+    #[test]
+    fn test_save() {
+        let conn = create_database().unwrap();
+        let _ = insert_password(&conn, "password_manager", "password123");
+        assert!(save_database(&conn, "test_username1").is_ok());
+        let _ = delete_database(&conn);
+        let load_result = load_database("test_username1");
+        assert!(load_result.is_ok());
+        let conn = load_result.unwrap();
+        assert!(get_password(&conn, "password_manager").is_ok());
+        assert!(get_password(&conn, "password_manager").unwrap() == "password123");
+        let _ = delete_user("test_username1");
+    }
+
+    // This tests if databases can be encryptd and decrypoted while maintaining data integrity
+    #[test]
+    fn test_encrypt() {
+        let conn = create_database().unwrap();
+        let _ = insert_password(&conn, "password_manager", "password123");
+        let _ = save_database(&conn, "test_username2");
+        let symmetric_key = crypto::csprng::<32>();
+        assert!(encrypt_database("test_username2", &symmetric_key).is_ok());
+        assert!(decrypt_database("test_username2", &symmetric_key).is_ok());
+        let conn = load_database("test_username2").unwrap();
+        assert!(get_password(&conn, "password_manager").is_ok());
+        assert!(get_password(&conn, "password_manager").unwrap() == "password123");
+        let _ = delete_user("test_username2");
+    }
+
+    // This test ensures that user information can be deleted correctly
+    #[test]
+    fn test_user_delete() {
+        let conn = create_database().unwrap();
+        let _ = insert_password(&conn, "password_manager", "password123");
+        let _ = get_password(&conn, "password_manager");
+        let _ = save_database(&conn, "test_username3");
+        let _ = encrypt_database("test_username3", &crypto::csprng::<32>());
+        assert!(delete_user("test_username3").is_ok());
+        assert!(Path::new("test_username3.db").exists() == false);
+        assert!(Path::new("test_username3.db.enc").exists() == false);
+        assert!(Path::new("test_username3_nonce.txt").exists() == false);
+    }
+
 }
