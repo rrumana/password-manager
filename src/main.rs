e34a808b52783bd9327ed28385d2e33bda0dc045
rrumana <ryan.rumana@gmail.com>
2024-04-19 03:01:31 -0700

    Added in looping input, simulating client/server relationship

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,17 @@ mod args;
 mod crypto;
 mod password;
 
+use read_input::prelude::*;
+
+use rpassword::read_password;
 use args::*;
 use password::*;
 use clap::Parser;
 use std::io::Write;
 use std::fs;
-use anyhow::{anyhow, Result};
+use std::io;
 use std::env;
+use anyhow::{anyhow, Result};
 use aes_gcm_siv::Nonce;
 
 //  TODO List:
@@ -29,164 +33,120 @@ use aes_gcm_siv::Nonce;
 //
 //  - Implement client side experience into browser extension
 
-fn _check_credentials(_username: &String, _password: &String) -> Result<()> {
-    // generate master password hash
-    //let master_key = crypto::kdf(username, password)?;
-    //let master_password_hash = crypto::kdf(password, &hex::encode(master_key))?;
+//////////////////////////////////////////////////////////////////////////////////////////////////
+// start input parsing
+
+
+
+
+
+
+// end input parsing
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+// start logging setup
+
+
+
+
+
+
+// end logging setup
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// start password manager functions
+
+// signup function
+fn signup_input() -> Result<()> {
+    // get username
+    // ensure it is long enough (8 characters)
+    // ensure it does not contain whitespace
+    let username = input::<String>()
+        .repeat_msg("Enter username: ")
+        .add_err_test(|x| x.len() >= 8, "Username must be at least 8 characters")
+        .add_err_test(|x| x.chars().any(|c| c.is_whitespace()), "Username must not contain whitespace")
+        .try_get()?;
+
+    // get password and force user to repeat it
+    // ensure it is long enough (10 characters)
+    // ensure it contains at least one digit
+    let mut password: String;
+    let mut password_confirm: String;
+
+    loop {
+        password = input::<String>()
+            .repeat_msg("Enter master password: ")
+            .add_err_test(|x| x.len() >= 10, "Master password must contain at least 10 characters")
+            .add_err_test(|x| x.chars().any(|c| c.is_digit(10)), "Master password must contain at least one digit")
+            .try_get()?;
+
+        password_confirm = input::<String>()
+            .repeat_msg("Confirm master password: ")
+            .try_get()?;
+
+        if password == password_confirm {
+            break;
+        } else {
+            println!("Error: Passwords do not match");
+        }
+    }
+
+    // pass username and password value onto the signup function
+    signup_handler(username, password)?;
+
+    // reutrn Ok if signup is successful
+    Ok(())
+}
+
+fn signup_handler(username: String, password: String) -> Result<()> {
+    // generate master key from user input using Argon2 kdf
+    let master_key = crypto::kdf(&username, &password)?;
     
-    // if map does not contain master_password_hash
-    // Map controlled by server side, make this first
-    //return Err(anyhow!("Incorrect username or password"));
- 
-    // if it does, return Ok(()), 
-    Ok(())
-}
-
-fn sign_up(username: &String, password: &String) -> Result<()> {
-    let master_key = crypto::kdf(username, password)?;
-    let master_password_hash = crypto::kdf(password, &hex::encode(master_key))?;
+    // stretch master key using HKDF
     let stretched_master_key = crypto::hkdf(&master_key)?;
-    let symmetric_key: [u8; 32] = crypto::csprng();
-    let iv: [u8; 12] = crypto::csprng();
+
+    // generate a random symmetric key and iv, use the IV to seed a nonce for AES-GCM
+    let symmetric_key = crypto::csprng::<32>();
+    let iv = crypto::csprng::<12>(); 
     let nonce = Nonce::from_slice(&iv);
-    let protected_symmetric_key: &[u8] = &crypto::encrypt_aes_gcm(&symmetric_key, &stretched_master_key, &nonce)?; 
 
-    // send stretched master key and protected symmetric key to server
+    // encrypt the symmetric key using the stretched master key
+    let protected_symmetric_key = crypto::encrypt_aes_gcm(&symmetric_key, &stretched_master_key, &nonce)?;
 
-    // create user database
-    let conn = create_database()?;
+    // pass the stretched master key, encryted key, and nonce to be stored for retrieval upon login
+    assign_key(&stretched_master_key, protected_symmetric_key.as_slice(), &nonce)?;
+    Ok(())
+}
 
-    // start user session
-
-    // return Ok if successful
+fn assign_key(stretched_master_key: &[u8; 32], protected_symmetric_key: &[u8], nonce: &Nonce) -> Result<()> {
     Ok(())
 }
 
 
-fn login(_username: &String, _password: &String) -> Result<()> {
-    // Check if the user exists
-    // Decrypt user database
-    // Start current user session
-    Ok(())
-}
-
-fn audit_session() -> Result<()> {
-    // print out session information
-    Ok(())
-}
-
-fn _logout(_username: &String, _password: &String) -> Result<()> {
-    // if username matches current session
-    // remove the current session
-    // save the database
-    // the decrypted database
-    // delete unencrypted database
-    // return Ok(())
-    Ok(())
-}
+// end password manager functions
+//////////////////////////////////////////////////////////////////////////////////////////////////
 
 fn main() -> Result<()> {
-    let env_key = "current_user";
-    
-    let mut current_user = String::new();
-    if !env::var(env_key).is_err() {
-        current_user = env::var(env_key)?;
+    println!("Welcome to Password Manager");
+
+    loop{
+        let command: String = match input().msg("Enter a command: ").try_get() {
+            Ok(input) => {
+                println!("You entered: {}", input);
+                input
+            }
+            Err(err) => {
+                println!("Error: Invalid input {}", err);
+                continue;
+            }
+        };
+
+        match command.as_str() {
+            "signup" => signup_input()?,
+            "login" => println!("You entered: login"),
+            _ => println!("Error: Invalid command"),
+        };
     }
-
-    let args = PwmParse::parse();
-
-    let _ = match args.command {
-        Command::Signup(args) => {
-            print!("Enter {}'s master password: ", args.username);
-            std::io::stdout().flush()?;
-            let password = rpassword::read_password()?;
-            sign_up(&args.username, &password)?;
-            current_user = args.username;
-            println!("Success, signed up user: {}!", current_user);
-        },
-        Command::Login(args) => {
-            print!("Enter {}'s master password: ", args.username);
-            std::io::stdout().flush()?;
-            let password = rpassword::read_password()?;
-            login(&args.username, &password)?;
-            current_user = args.username;
-            println!("Logged in as {}", current_user);
-        },
-        Command::Session(args) => {
-            if current_user.is_empty() { return Err(anyhow!("You must be logged in to audit a session")); }
-            print!("Enter {}'s master password: ", current_user);
-            std::io::stdout().flush()?;
-            let _password = rpassword::read_password()?;
-            if args.audit == Some(true) { audit_session()?; }
-            else { println!("Logged in as {}", current_user); }
-        },
-        Command::Logout(args) => {
-            print!("Enter {}'s master password: ", args.username);
-            std::io::stdout().flush()?;
-            let _password = rpassword::read_password()?;
-            // TODO: handle logout request
-            println!("Logging out user {}", args.username);
-        },
-        Command::Add(add)  => {
-            println!("Command: {:?}", add);
-            let service = add.service_name;
-            let password = add.password;
-            println!("Service: {}", service);
-            println!("Password: {}", password);
-            // ask user for master key
-            // if not authenticated:
-                // inform user of incorrect password, quit
-            // else:
-                // search for service in database
-                // if found
-                    // inform user that service already exists
-                    // Display existing password
-                    // if they decide to keep existing password
-                        // acknowledge, then quit 
-                    // else:
-                        // acknowledge, update password, then quit
-                // else:
-                    // add service and password to database
-                // inform user of the result
-        },
-        Command::Get(get) => {
-            println!("Command: {:?}", get);
-            // ask user for master key
-            // if not authenticated, inform user of incorrect password, quit
-            // if authenticated:
-            // search for service in database
-            // if found, decrypt and display password
-            // else inform user that service was not found
-        },
-        Command::Delete(delete) => {
-            println!("Command: {:?}", delete);
-        },
-        Command::List(list) => {
-            println!("Command: {:?}", list);
-        },
-    };
-
-    env::set_var(env_key, current_user);
-    if !env::var(env_key).is_err() {
-        println!("Current user: {}", env::var(env_key)?);
-    }
- 
-    Ok(())
-}
-
-fn start_client() -> Result<()> {
-
-    Ok(())
-}
-
-fn start_server() -> Result<()> {
-    Ok(())
-}
-
-fn stop_client() -> Result<()> {
-    Ok(())
-}
-
-fn stop_server() -> Result<()> {
-    Ok(())
 }
